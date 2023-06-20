37f4828f2bed4b0427ba57f4f963ac7d651994c0
usausa <machi.pon@gmail.com>
2023-06-20 09:49:42 +0900

    Update project baseline

--- Template.BlazorWasm/Client/Program.cs
+++ Template.BlazorWasm/Client/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+using MudBlazor;
+using MudBlazor.Services;
+
 using Template.BlazorWasm.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,5 +11,18 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMudServices(config =>
+{
+    // Snackbar
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 3000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Outlined;
+});
 
 await builder.Build().RunAsync();
