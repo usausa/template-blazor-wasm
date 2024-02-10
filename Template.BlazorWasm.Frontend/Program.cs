7957aa1c7b92932e7bb4739a6c0fbf12f990ac1d
usausa <machi.pon@gmail.com>
2024-02-10 17:18:33 +0900

    Update

--- Template.BlazorWasm.Frontend/Program.cs
+++ Template.BlazorWasm.Frontend/Program.cs
@@ -1,9 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-using MudBlazor;
-using MudBlazor.Services;
-
 using Template.BlazorWasm.Frontend;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -11,18 +8,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddMudServices(config =>
-{
-    // Snackbar
-    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
-    config.SnackbarConfiguration.PreventDuplicates = false;
-    config.SnackbarConfiguration.NewestOnTop = false;
-    config.SnackbarConfiguration.ShowCloseIcon = true;
-    config.SnackbarConfiguration.VisibleStateDuration = 3000;
-    config.SnackbarConfiguration.HideTransitionDuration = 500;
-    config.SnackbarConfiguration.ShowTransitionDuration = 500;
-    config.SnackbarConfiguration.SnackbarVariant = Variant.Outlined;
-});
 
 await builder.Build().RunAsync();
