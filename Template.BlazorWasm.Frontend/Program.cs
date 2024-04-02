7305543b35b9f5d4e0bc22b40d0b1f1f3623a612
usausa <machi.pon@gmail.com>
2024-04-02 17:16:10 +0900

    Update folder

--- Template.BlazorWasm.Frontend/Program.cs
+++ Template.BlazorWasm.Frontend/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 
-using Template.BlazorWasm.Frontend;
+using Template.BlazorWasm.Frontend.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
