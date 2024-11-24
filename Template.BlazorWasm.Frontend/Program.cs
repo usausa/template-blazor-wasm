1108a091ddaf663dff51471d8927c3aad18dbdf3
usausa <machi.pon@gmail.com>
2024-11-24 15:53:25 +0900

    Update setting

--- Template.BlazorWasm.Frontend/Program.cs
+++ Template.BlazorWasm.Frontend/Program.cs
@@ -1,10 +1,8 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 
-using Template.BlazorWasm.Frontend.Components;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<Template.BlazorWasm.Frontend.Components.App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
