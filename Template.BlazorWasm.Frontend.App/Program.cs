3d6a66f05ac6c4440c24f5beb3ff48ac50f24d9a
usausa <machi.pon@gmail.com>
2026-08-25 09:58:53 +0900

    Update

--- Template.BlazorWasm.Frontend.App/Program.cs
+++ Template.BlazorWasm.Frontend.App/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.FluentUI.AspNetCore.Components;
 
+using Microsoft.FluentUI.AspNetCore.Components;
 
 //--------------------------------------------------------------------------------
 // Configure builder
