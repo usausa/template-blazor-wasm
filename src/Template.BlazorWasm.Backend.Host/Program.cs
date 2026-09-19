f609e3990f487fba07d81a5030457b9e3eb865bc
usausa <machi.pon@gmail.com>
2026-09-19 15:06:08 +0900

    Update

--- src/Template.BlazorWasm.Backend.Host/Program.cs
+++ src/Template.BlazorWasm.Backend.Host/Program.cs
@@ -49,14 +49,17 @@ app.LogStartupInformation();
 // Forwarded headers
 app.UseForwardedHeaders();
 
+// W3C log
+app.UseW3CLog();
+
 // Error handler
 app.UseErrorHandler();
 
 // Compression
 app.UseCompression();
 
-// Logging
-app.UseLogging();
+// HTTP log
+app.UseHttpLog();
 
 // Static files (Blazor WebAssembly assets)
 app.UseBlazorFrameworkFiles();
@@ -65,9 +68,6 @@ app.UseStaticFiles();
 // Authentication
 app.UseAuthentication();
 app.UseAuthorization();
-
-// Logging context
-app.UseLoggingContext();
 
 // End point
 app.MapEndpoints();
